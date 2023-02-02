8cf2884f7b56f3e704603006e417a4bcf5b0c9f6
FelixB02 <felix.blechl@chello.at>
2023-02-02 11:56:10 +0100

    Challenge 2nd way

--- Felix/Challenge.js
+++ Felix/Challenge.js
@@ -3,15 +3,16 @@ function timeConvert(minutes) {
     let hours = Math.floor(minutes / 60);
     let minutesr = minutes % 60
 
-    let result = [hours, minutesr, minutess]
+    // let result = [hours, minutesr, minutess]
+    console.log(`${minutess} minutes = ${hours} hour(s) and ${minutesr} minute(s).`)
     return (result)
 }
 
 
 
-let res = timeConvert(200);
-let res1 = timeConvert(500);
-let res2 = timeConvert(10000);
-console.log(`${res[2]} minutes = ${res[0]} hour(s) and ${res[1]} minutes(s).`)
-console.log(`${res1[2]} minutes = ${res1[0]} hour(s) and ${res1[1]} minutes(s).`)
-console.log(`${res2[2]} minutes = ${res2[0]} hour(s) and ${res2[1]} minutes(s).`)
+// let res = timeConvert(200);
+// let res1 = timeConvert(500);
+// let res2 = timeConvert(10000);
+// console.log(`${res[2]} minutes = ${res[0]} hour(s) and ${res[1]} minutes(s).`)
+// console.log(`${res1[2]} minutes = ${res1[0]} hour(s) and ${res1[1]} minutes(s).`)
+// console.log(`${res2[2]} minutes = ${res2[0]} hour(s) and ${res2[1]} minutes(s).`)
